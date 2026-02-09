d4451cfb8e461f75b9f1ede31d90219431707421
Isadora Rates <dorarates19@gmail.com>
2026-02-09 17:08:35 -0300

    Programa básico em python

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Aleatório")
+nome = input("Digite seu nome: ")
+salario = float(input("Digite seu salário: "))
+bonus = float(input("Digite o valor do bônus: "))
+print("Olá, " + nome + ". Seu salário total é de: R$" + str(1000 + salario * bonus) + ".")
